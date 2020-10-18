7559e020e957d0503bff73f86454a89e77d62978
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-17 23:32:35 -0500

    got the first half of employee test to run without errors

--- library/employee.js
+++ library/employee.js
@@ -1,1 +1,36 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name, id, email, role) {
+    
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.role = role;
+    }
+};
+const getName = name => this.name;
+const getId();
+const getEmail();
+const getRole();
+getName(name);
+
+
+
+
+
+    module.exports = Employee;
+
+    // if (typeof name !== "string" || !name.trim().length) {
+    //     throw new Error("Expected parameter 'name' to be a non-empty string");
+    //   }
+    
+    //   if (typeof id !== "number" || isNaN(id) || id < 0) {
+    //     throw new Error("Expected parameter 'id' to be a non-negative number");
+    //   }
+
+    //   if (typeof email !== "string" || !email.trim().length) {
+    //     throw new Error("Expected parameter 'email' to be a non-empty string");
+    //   }
+    //   if (typeof role !== "string" || !role.trim().length) {
+    //     throw new Error("Expected parameter 'role' to be a non-empty string");
+    //   }
