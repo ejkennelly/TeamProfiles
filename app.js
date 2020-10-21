a78bb1a78c8e41765ddeb7fbf66adbbbad8bd854
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-20 20:28:14 -0500

    finished js

--- app.js
+++ app.js
@@ -138,6 +138,18 @@ function addHTML(member) {
             </ul>
             </div>
         </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
         } else {
             const officePhone = member.getOfficeNumber();
             data = `<div class="col-6">
